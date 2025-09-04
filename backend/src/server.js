816dfb864f8fd1b5c5b4ef3eaf63bba08f9e6aa5
osamaelsaeed <om35453@gmail.com>
2025-09-04 11:38:49 +0300

    auth implemented clerk inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,40 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
+
+//access req body
+app.use(express.json());
+
+//req.auth will be avalilable in the req object
+app.use(clerkMiddleware());
+
+//allow to use ingest functions
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server started on port ${ENV.PORT}`);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server started on port ${ENV.PORT}`);
+        connectDB();
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
